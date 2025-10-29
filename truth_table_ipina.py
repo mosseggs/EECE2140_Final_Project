bb122be31ff0cc4a1e5340ad9ae7e80fa46ea0a0
Gabriel Lau <lau.ga@northeastern.edu>
2025-10-28 21:03:07 -0400

    Can parse through parenthesis now, next step is auto generating answers.

--- truth_table_ipina.py
+++ truth_table_ipina.py
@@ -8,53 +8,80 @@ class TruthTable:
     """makes a truthtable"""
 
     def __init__(self) -> None:
-        self.variables : list[str] = []
-        self.table : list[dict[str, int]] = []
+        self._variables : list[str] = []
+        self._table : list[dict[str, int]] = []
+        self._answers : list[str] = []
+
+    @property
+    def variables(self) -> list[str]:
+        return self._variables
+
+    @property
+    def answers(self) -> list[str]:
+        return self._answers
 
     def parse(self, phrase : str) -> None:
         """Parses the string for the phrases."""
         # TODO: Needs work
-        for char in phrase.split(" "):
-            if char.lower() not in ["and", "or", "xor", "not", "nand", "nor", "xnor"]:
-                self.variables.append(char)
+        while(phrase.find("(") != -1):
+            print(phrase)
+            phrase = self.subPhrases(phrase)
 
-    def get_variable_count(self) -> None:
-        """Gets the amount of variables for manual insertion into the truth table"""
-        while True:
-            count = int(input("Enter number of variables (1 - 4): "))
-            while count > 0:
-                print(count)
-                count2 : int = count
-                if count >= 26:
-                    count2 = 26
-                self.add_variables(count2, int(count/26))
-                count -= 26
-            break
-        print(f"{self.variables}")
+        print(phrase)
+        for char in phrase.split(" "):
+            if char.lower() not in ["and", "or", "xor", "not", "nand", "nor", "xnor", ""]:
+                self._variables.append(char)
+
+    def subPhrases(self, phrase: str) -> str:
+        """Parses the string for any sub-phrases using parenthesis"""
+        start : int = 0
+        subPhrase : str = ""
+        for i in range(len(phrase)):
+            if phrase[i] == "(":
+                start += 1
+            elif phrase[i] == ")" and start != 1:
+                start -= 1
+            elif phrase[i] == ")" and start == 1:
+                sub_table : TruthTable = TruthTable()
+                sub_table.parse(phrase[phrase.find("(") + 1: i])
+                if(sub_table.variables != []):
+                    print("---" + phrase)
+                    print(self.variables)
+                    print(sub_table.variables)
+                    self._variables += sub_table.variables
+                    print(self.variables)
+                    print("---")
+                subPhrase = phrase[:phrase.find("(")] + phrase[i + 1:]
+                break
+        return subPhrase
+
+
+
+
 
     def add_variables(self, count: int, start: int) -> None:
         """Helper method to add variables over 26"""
         for letter in range(count):
             if start > 0:
-                self.variables.append(chr(start+65))
-            self.variables.append(chr(letter + 65))
+                self._variables.append(chr(start+65))
+            self._variables.append(chr(letter + 65))
 
     def generate_table(self)-> None:
         """Generates a table"""
-        total_rows = 2 ** len(self.variables)
+        total_rows = 2 ** len(self._variables)
 
         for i in range(total_rows):
             row = {}
             print(f"Entry {i + 1}:")
-            for var in self.variables:
+            for var in self._variables:
                 row[var] = 0
-            for j in range(len(self.variables)):
-                k = len(self.variables) - j - 1
+            for j in range(len(self._variables)):
+                k = len(self._variables) - j - 1
                 print(f"{i}:{j}:{int(i/(2**j))}")
                 if int(i / (2**j)) % 2 == 0:
-                    row[list(self.variables)[k]] = 0
+                    row[list(self._variables)[k]] = 0
                 else:
-                    row[list(self.variables)[k]] = 1
+                    row[list(self._variables)[k]] = 1
             # TODO: make this get filled in automatically.
             while True:
                 y_value = input("Enter output Y (0 or 1): ")
@@ -64,20 +91,22 @@ class TruthTable:
                 else:
                     print("Invalid input. Enter 0 or 1.")
             print()
-            self.table.append(row)
+            self._table.append(row)
 
     def display_table(self)-> None:
         """Prints the table"""
         print("\nTruth Table:")
-        print("-" * (len(self.variables) * 4 + 6))
-        header = " ".join(self.variables) + " | Y"
+        print("-" * (len(self._variables) * 4 + 6))
+        header = " ".join(self._variables) + " | Y"
         print(header)
-        print("-" * (len(self.variables) * 4 + 6))
-        for row in self.table:
-            values = " ".join(str(row[var]) for var in self.variables)
-            print(f"{values} | {row['Y']}")
-        print("-" * (len(self.variables) * 4 + 6))
+        print("-" * (len(self._variables) * 4 + 6))
+        for row in self._table:
+            values : str = ""
+            for var in self._variables:
+                values += str(row[var]) + " " * len(var)
+            print(f"{values}| {row['Y']}")
+        print("-" * (len(self._variables) * 4 + 6))
 table = TruthTable()
-table.parse("A AND B")
+table.parse("((AC AND B) AND C)")
 table.generate_table()
 table.display_table()
